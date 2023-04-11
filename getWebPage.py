d18e15a4c4d84c54cd16c5308c27a34ac641319c
Fei20160811 <paradox0912gmail.com>
2023-04-11 16:30:18 +0800

    爬蟲增加隨機暫停時間

--- getWebPage.py
+++ getWebPage.py
@@ -6,10 +6,15 @@ Created on Thu Mar 30 16:19:59 2023
 @author: ml
 """
 import requests
+import random
+import time
 
 def get_web_page(url):
     resp = requests.get(url = url)
-                        #cookies = {'over18':'1'}) #針對ptt 18歲許可跳窗所需參數
+                        #cookies = {'over18':'1'}) #針對ptt 18歲許可跳窗所需
+    #隨機暫停1～5秒
+    time.sleep(random.uniform(1, 5))
+                    
     if resp.status_code != 200:
         print(resp.status_code)
         print('Invalid url:', resp.url)
--- jobAnalysis.py
+++ jobAnalysis.py
@@ -8,9 +8,25 @@ Created on Mon Apr 10 12:46:50 2023
 
 import pandas as pd
 import os
+import csv
 
 def job_analysis(date):
-    df_jobs = pd.read_json('job_'+ date.lstrip('0') +'.json')
-    print(df_jobs.index)
+    output_list = []
+    df_jobs = pd.read_json('job_'+ date.lstrip('0') +'.json', orient='records')
+    df_jobs = [job_arr for index in df_jobs for job_arr in df_jobs[index] if job_arr != None]
+    df_jobs = pd.DataFrame(df_jobs)
+    df_jobs["company_addr"] = df_jobs["company_addr"].str[:3]
+    df_jobs = df_jobs[df_jobs["company_addr"].isin(['台北市' ,'新北市', '新竹市', '新竹縣'])]
+       
+    print(df_jobs.groupby(["company_name", "company_desc"]))
+    for key, item in df_jobs.groupby(["company_name", "company_desc"]):
+        job_row = (key[0], key[1], item["job_name"], item["company_addr"])
+        output_list.append(job_row)
+        
+    with open('job_'+ date.lstrip('0') +'.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['公司名稱', '公司產品','職稱', '公司所在縣市'])
+        for item in output_list:
+            writer.writerow(item)
     
-job_analysis('0410')
+job_analysis('0411')
